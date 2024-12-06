0f8349fecc5c200867eebe734cd1e50c387bcddd
lars34932 <34932@ma-web.nl>
2024-12-06 12:04:49 +0100

    port 81

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const app = express();
-const PORT = 80;
+const PORT = 81;
 
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
