24a6e246417e602031d44f39e43a45378001e1e8
lars34932 <34932@ma-web.nl>
2024-12-16 12:05:40 +0100

    return hash and salt

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.post('/login', async (req, res) => {
                 res.json({ success: false, error: error.message });
                 return;
             }
-            
-            console.log(results);
+
+            res.json({ success: false, hash: results.password, salt: results.salt, error: null });
         });
     });
 })
